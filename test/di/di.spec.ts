0305961c45eaa6b6942fec6330bb3fb2659546cd
mazulatas <mazulatas@gmail.com>
2020-07-08 23:22:50 +0300

    fix test

--- test/di/di.spec.ts
+++ test/di/di.spec.ts
@@ -1,17 +1,22 @@
-import { Inject, Injectable, Injector } from '../../src/di'
+import { Inject, Injectable } from '../../src/di'
 
 describe('DI', () => {
   it('should ddd', () => {
     @Injectable()
     class TestClass1 {}
+
     @Injectable()
     class TestClass2 {
-      @Inject(TestClass1) public testField?: TestClass1
+      @Inject(TestClass1) public testField: TestClass1
     }
+
     @Injectable()
     class TestClass3 {
-      @Inject(TestClass2) public testField2?: TestClass2
+      @Inject(TestClass2) public testField2: TestClass2
     }
-    const injector = Injector.getInjector(TestClass3)
+
+    const instance = new TestClass3()
+    expect(instance.testField2).toBeInstanceOf(TestClass2)
+    expect(instance.testField2.testField).toBeInstanceOf(TestClass1)
   })
 })
